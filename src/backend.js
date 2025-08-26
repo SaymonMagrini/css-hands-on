456b3741c377b6e2fe4499803db52a9363019d30
SaymonMagrini <saymomagrini@gmail.com>
2025-08-25 21:08:52 -0300

    estilizado e criado botão

--- src/backend.js
+++ src/backend.js
@@ -1,14 +1,23 @@
-let app=document.getElementById('app');
-
+// stating variables for the heads
+let app = document.getElementById('cabeças');
 let count = 0;
 
 function loopcount() {
     for (; count <= 69; count++) {
         console.log(`counter is: ${count}`);
         let h1 = document.createElement('h1');
-        h1.textContent = `header ${count}`;
+        h1.textContent = `cabeça ${count}`;
         app.appendChild(h1);
     }
 }
 
 loopcount();
+
+function goUp() {
+    window.scrollTo({
+        top: 0,
+        behavior: 'smooth'  // smooth scrolling effect
+    });
+}
+
+butao = document.getElementById('botao').addEventListener('click', goUp);
